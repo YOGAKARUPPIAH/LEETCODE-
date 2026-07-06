73b890ea346df795a745f6b73cc4410ed46e646f
YOGA K <yogak010806@gmail.com>
2026-07-06 16:29:42 +0530

    Time: 29 ms (5.60%) | Memory: 107.1 MB (11.96%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer>set=new HashSet<>();
+     /*   HashSet<Integer>set=new HashSet<>();
         for(int i=0;i<nums.length;i++){
             if(set.contains(nums[i])){
                 return true;
             }
             set.add(nums[i]);
         }
+                return false; */
+                HashMap<Integer,Integer>map=new HashMap<>();
+                
+                for(int x:nums){
+                    map.put(x,map.getOrDefault(x,0)+1);
+                
+                if(map.get(x)>1){
+                    return true;
+                }}
                 return false;
               }
+
         }
