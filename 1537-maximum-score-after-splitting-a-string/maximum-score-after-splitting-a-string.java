22dd48c28f7f2a957df0cbeaf4e9ab0833dad1fc
YOGA K <yogak010806@gmail.com>
2026-09-24 17:54:18 +0530

    Time: 12 ms (5.56%) | Memory: 42.8 MB (50.46%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxScore(String s) {
+        
+
+        int max=0;
+       
+        for(int i=0;i<s.length()-1;i++){
+            int one=0;
+            int zer=0;
+
+            for(int j=0;j<=i;j++){
+                if(s.charAt(j)=='0'){
+                    zer++;
+                }
+            }
+
+            for(int j=i+1;j<s.length();j++){
+                if(s.charAt(j)=='1'){
+                    one++;
+                }
+            }
+            int score=one+zer;
+            max=Math.max(score,max);
+        }
+        return max;
+    }
+}
